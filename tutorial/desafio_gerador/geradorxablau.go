a00763739ce3c387aff2247fed67b08cf13fae00
luiz.campos <luiz.campos@sinqia.com.br>
2019-07-04 13:26:32 -0300

    Començando gerador de xablau

---
+++ tutorial/desafio_gerador/geradorxablau.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func gerador_xablau() {
+
+}
+
+func main() {
+	fmt.Printf("Teste")
+}
+
+// "quêêêêêê",
+// "perc",
+// "fiforn",
+// "tipoType",
+// "énada",
+// "percAss",
+// "vouBotarNaSuaBranch",
+// "tranquera",
+// "canalha",
+// "xablau",
+// "gcp",
+// "putz",
+// "chorando",
+// "valValor",
+// "trocaBem",
+// "caaaraaamba",
+// "taCerto",
+// "QuatroEvinte",
+// "naoSePodeGanharTodas",
+// "aceiteMinhaDerrotaPoisEuJaAceitei"
