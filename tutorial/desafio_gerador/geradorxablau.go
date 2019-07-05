eebe56391cd1d1b6186c09ebdc92f18abf958af0
luiz.campos <luiz.campos@sinqia.com.br>
2019-07-05 18:40:16 -0300

    COmeçando gerador de xablau

--- tutorial/desafio_gerador/geradorxablau.go
+++ tutorial/desafio_gerador/geradorxablau.go
@@ -2,31 +2,52 @@ package main
 
 import "fmt"
 
-func gerador_xablau() {
+func gerador_xablau() string {
+	primes := []string{"quêêêêêê",
+	"perc",
+	"fiforn",
+	"tipoType",
+	"énada",
+	"percAss",
+	"vouBotarNaSuaBranch",
+	"tranquera",
+	"canalha",
+	"xablau",
+	"gcp",
+	"putz",
+	"chorando",
+	"valValor",
+	"trocaBem",
+	"caaaraaamba",
+	"taCerto",
+	"QuatroEvinte",
+	"naoSePodeGanharTodas",
+	"aceiteMinhaDerrotaPoisEuJaAceitei"}
 
+	return primes.shuffle
 }
 
 func main() {
 	fmt.Printf("Teste")
 }
 
-// "quêêêêêê",
-// "perc",
-// "fiforn",
-// "tipoType",
-// "énada",
-// "percAss",
-// "vouBotarNaSuaBranch",
-// "tranquera",
-// "canalha",
-// "xablau",
-// "gcp",
-// "putz",
-// "chorando",
-// "valValor",
-// "trocaBem",
-// "caaaraaamba",
-// "taCerto",
-// "QuatroEvinte",
-// "naoSePodeGanharTodas",
-// "aceiteMinhaDerrotaPoisEuJaAceitei"
+"quêêêêêê",
+"perc",
+"fiforn",
+"tipoType",
+"énada",
+"percAss",
+"vouBotarNaSuaBranch",
+"tranquera",
+"canalha",
+"xablau",
+"gcp",
+"putz",
+"chorando",
+"valValor",
+"trocaBem",
+"caaaraaamba",
+"taCerto",
+"QuatroEvinte",
+"naoSePodeGanharTodas",
+"aceiteMinhaDerrotaPoisEuJaAceitei"
