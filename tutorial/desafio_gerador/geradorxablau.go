bcee3e6c143bd8df652164b88819948ca614dda9
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2019-07-10 22:54:01 -0300

    Gerador de xablau quase concluido

--- tutorial/desafio_gerador/geradorxablau.go
+++ tutorial/desafio_gerador/geradorxablau.go
@@ -1,53 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func gerador_xablau() string {
+func geradorxablau() []string {
 	primes := []string{"quêêêêêê",
-	"perc",
-	"fiforn",
-	"tipoType",
-	"énada",
-	"percAss",
-	"vouBotarNaSuaBranch",
-	"tranquera",
-	"canalha",
-	"xablau",
-	"gcp",
-	"putz",
-	"chorando",
-	"valValor",
-	"trocaBem",
-	"caaaraaamba",
-	"taCerto",
-	"QuatroEvinte",
-	"naoSePodeGanharTodas",
-	"aceiteMinhaDerrotaPoisEuJaAceitei"}
+		"perc",
+		"fiforn",
+		"tipoType",
+		"énada",
+		"percAss",
+		"vouBotarNaSuaBranch",
+		"tranquera",
+		"canalha",
+		"xablau",
+		"gcp",
+		"putz",
+		"chorando",
+		"valValor",
+		"trocaBem",
+		"caaaraaamba",
+		"taCerto",
+		"QuatroEvinte",
+		"naoSePodeGanharTodas",
+		"aceiteMinhaDerrotaPoisEuJaAceitei"}
 
-	return primes.shuffle
+	return shuffle(primes)
+}
+
+func shuffle(vals []string) []string {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	ret := make([]string, len(vals))
+	n := len(vals)
+	for i := 0; i < n; i++ {
+		randIndex := r.Intn(len(vals))
+		ret[i] = vals[randIndex]
+		vals = append(vals[:randIndex], vals[randIndex+1:]...)
+	}
+	return ret
 }
 
 func main() {
-	fmt.Printf("Teste")
+	fmt.Println(geradorxablau())
 }
-
-"quêêêêêê",
-"perc",
-"fiforn",
-"tipoType",
-"énada",
-"percAss",
-"vouBotarNaSuaBranch",
-"tranquera",
-"canalha",
-"xablau",
-"gcp",
-"putz",
-"chorando",
-"valValor",
-"trocaBem",
-"caaaraaamba",
-"taCerto",
-"QuatroEvinte",
-"naoSePodeGanharTodas",
-"aceiteMinhaDerrotaPoisEuJaAceitei"
